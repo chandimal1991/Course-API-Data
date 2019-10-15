7f5931a2fc07e36f0bd4a2909a3f04e106bc4d51
chandimal1991 <37394684+chandimal1991@users.noreply.github.com>
2019-10-15 14:11:27 +0530

    Add Project

---
+++ course-api-data/src/main/java/com/hr/springbootstarter/topic/TopicRepository.java
@@ -0,0 +1,8 @@
+package com.hr.springbootstarter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+	
+
+}
